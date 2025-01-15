d449f70607be040190a67e73d44108095d8728aa
yanakorsak <kazantip2310@gmail.com>
2025-01-15 14:25:31 +0100

    up

--- operadores/main.js
+++ operadores/main.js
@@ -5,3 +5,9 @@ const price =100
 if( inStock===true && (onSale===true || price < 80)){
   alert ("buy!!!")
 }
+
+
+const temp = 83
+const willRain =true
+const humid =(willRain ===true && temp>80)
+console.log (humid)
